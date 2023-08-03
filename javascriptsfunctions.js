2757c5434332f4c413320e4758cd13028caec276
Alberto Cándido <103060171+Aprilis0104@users.noreply.github.com>
2023-08-03 13:10:05 +0200

    Actualizar javascriptsfunctions.js

--- javascriptsfunctions.js
+++ javascriptsfunctions.js
@@ -7,3 +7,25 @@ function myFunction() {
       x.className = "header";
     }
   }
+
+
+
+
+function PlantRandomizer() {
+      // Obtener el valor ingresado por el usuario
+      var palabraIngresada = document.getElementById('idBiome').value;
+
+      // Llamar a la función y obtener el resultado
+      var result = procesarPalabra(palabraIngresada);
+
+      // Mostrar el resultado en el párrafo con id "resultado"
+      document.getElementById('result').textContent = result;
+    }
+
+    function procesarPalabra(palabra) {
+      // Hacer alguna operación con la palabra ingresada (ejemplo: convertir a mayúsculas)
+      var result = palabra.toUpperCase();
+
+      // Devolver el resultado
+      return result;
+}
